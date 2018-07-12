d690b2317dbe072358403295062ba4a9c29127c5
Nick Carpenetti <nickcarpenetti@gmail.com>
2018-07-12 15:24:53 -0700

    Object Loading project begun

---
+++ 7-ObjLoading/RenderSystem.h
@@ -0,0 +1,163 @@
+#pragma once
+
+//vulkan, glfw
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+//glm
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+//STL
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <chrono>
+
+//ubm-vk
+#include "FileIO.h"
+#include "Validation.h"
+#include "Extensions.h"
+#include "VulkanContext.h"
+#include "QueueFamilies.h"
+#include "CommandPool.h"
+#include "BufferManager.h"
+#include "ImageManager.h"
+#include "Swapchain.h"
+#include "Texture.h"
+#include "Vertex.h"
+
+
+
+const std::vector<Vertex> doubleSquareVertices = {
+	{ { -0.5f, -0.5f, 0.0f },{ 1.0f, 0.0f, 0.0f },{ 0.0f, 0.0f } },
+	{ { 0.5f, -0.5f, 0.0f },{ 0.0f, 1.0f, 0.0f },{ 1.0f, 0.0f } },
+	{ { 0.5f, 0.5f, 0.0f },{ 0.0f, 0.0f, 1.0f },{ 1.0f, 1.0f } },
+	{ { -0.5f, 0.5f, 0.0f },{ 1.0f, 1.0f, 1.0f },{ 0.0f, 1.0f } },
+
+	{ { -0.5f, -0.5f, -0.5f },{ 1.0f, 0.0f, 0.0f },{ 0.0f, 0.0f } },
+	{ { 0.5f, -0.5f, -0.5f },{ 0.0f, 1.0f, 0.0f },{ 1.0f, 0.0f } },
+	{ { 0.5f, 0.5f, -0.5f },{ 0.0f, 0.0f, 1.0f },{ 1.0f, 1.0f } },
+	{ { -0.5f, 0.5f, -0.5f },{ 1.0f, 1.0f, 1.0f },{ 0.0f, 1.0f } }
+};
+
+const std::vector<uint16_t> doubleSquareIndices = {
+	0, 1, 2, 2, 3, 0,
+	4, 5, 6, 6, 7, 4
+};
+
+struct UniformBufferObject {
+	glm::mat4 model;
+	glm::mat4 view;
+	glm::mat4 proj;
+};
+
+const int MAX_CONCURRENT_FRAMES = 2;
+const std::string VERT_SHADER_PATH = "shaders/square_vert.spv";
+const std::string FRAG_SHADER_PATH = "shaders/square_frag.spv";
+const std::string TEXTURE_PATH = "textures/texture.jpg";
+
+class RenderSystem
+{
+public:
+	RenderSystem() {}
+	~RenderSystem() {}
+
+	void initialize(GLFWwindow *window);
+	void drawFrame();
+	void cleanup();
+
+	void setClearColor(VkClearValue clearColor);
+
+private:
+	std::shared_ptr<VulkanContext> mContext;
+	std::shared_ptr<CommandPool> mCommandPool;
+	std::shared_ptr<BufferManager> mBufferManager;
+	std::shared_ptr<ImageManager> mImageManager;
+
+	//Swapchain Setup
+	std::unique_ptr<Swapchain> mSwapchain;
+
+	//Command Buffers
+	std::vector<VkCommandBuffer> mCommandBuffers;
+
+#pragma region Pipeline
+	//more closely attached to a renderpass than swapchain
+	std::vector<VkFramebuffer> mSwapchainFramebuffers;
+	
+	//Pipeline
+	VkRenderPass mRenderPass;
+	VkDescriptorSetLayout mDescriptorSetLayout;
+	VkDescriptorPool mDescriptorPool;
+	std::vector<VkDescriptorSet> mDescriptorSets;
+	VkPipelineLayout mPipelineLayout;
+	VkPipeline mPipeline;
+#pragma endregion
+	//depth buffer
+	VkImage mDepthImage;
+	VkFormat mDepthImageFormat;
+	VkDeviceMemory mDepthImageMemory;
+	VkImageView mDepthImageView;
+
+	//Rendering Synchronization primitives
+	std::vector<VkSemaphore> mImageAvailableSemaphores;
+	std::vector<VkSemaphore> mRenderFinishedSemaphores;
+	std::vector<VkFence> mFrameFences;
+	size_t mCurrentFrame = 0;
+
+	VkClearValue mClearColor = { 0.0f, 0.5f, 0.5f, 1.0f };
+	glm::mat4 model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+
+#pragma region Buffers
+	//vertex buffer
+	VkBuffer mVertexBuffer;
+	VkDeviceMemory mVertexBufferMemory;
+
+	//index buffer
+	VkBuffer mIndexBuffer;
+	VkDeviceMemory mIndexBufferMemory;
+
+	//ubo
+	std::vector<VkBuffer> mUniformBuffers;
+	std::vector<VkDeviceMemory> mUniformBuffersMemory;
+#pragma endregion
+
+	Texture* mTexture;
+
+private:
+	//void createInstance();
+	void createDevice();
+	void createSurface(GLFWwindow* window);
+	
+	//SWAPCHAIN
+	void createSwapchain();
+	void recreateSwapchain();
+	void cleanupSwapchain();
+
+	//Pipeline
+	void createGraphicsPipeline();
+	VkShaderModule createShaderModule(const std::vector<char>& code);
+	void createRenderPass();
+
+	//Descriptors
+	void createDescriptorSetLayout();
+	void createDescriptorPool();
+	void createDescriptorSets();
+
+	//Command Buffers
+	void createFramebuffers();
+	void createCommandPool();
+	void createCommandBuffers();
+	void createDepthBuffer();
+	void createSyncObjects();
+
+
+	void createMesh(const std::vector<Vertex> &vertices, const std::vector<uint16_t> &indices);
+	void createTexture(const std::string &filename);
+	void createUniformBufferObject();
+	void updateUniformBuffer(uint32_t currentImage);
+};
---
+++ 7-ObjLoading/VulkanContext.cpp
@@ -0,0 +1,207 @@
+#include "VulkanContext.h"
+
+VulkanContext::VulkanContext() : 
+	window(nullptr),
+	mCallback(nullptr),
+	mInstance(VK_NULL_HANDLE),
+	device(VK_NULL_HANDLE),
+	graphicsQueue(VK_NULL_HANDLE),
+	presentQueue(VK_NULL_HANDLE),
+	surface(VK_NULL_HANDLE)
+{}
+
+void VulkanContext::initialize(GLFWwindow *window)
+{
+	std::cout << "Initializing Vulkan Context" << std::endl;
+	createInstance();
+	
+	if (enableValidationLayers) {
+		setupDebugCallback();
+	}
+
+	this->window = window;
+	createSurface(mInstance, window);
+	createDevice(mInstance);
+}
+
+void VulkanContext::cleanup()
+{
+	std::cout << "Destroying Vulkan Context" << std::endl;
+
+	vkDestroyDevice(device, nullptr);
+	vkDestroySurfaceKHR(mInstance, surface, nullptr);
+
+	if (enableValidationLayers) {
+		DestroyDebugReportCallbackEXT(mInstance, mCallback, nullptr);
+	}
+
+	vkDestroyInstance(mInstance, nullptr);
+}
+
+uint32_t VulkanContext::findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties)
+{
+	VkPhysicalDeviceMemoryProperties memProperties;
+	vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+	for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+		if (typeFilter & (1 << i) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+			return i;
+		}
+	}
+
+	throw std::runtime_error("Failed to find a suitable memory type!");
+}
+
+void VulkanContext::createInstance()
+{
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "5-BigRefactor";
+	appInfo.applicationVersion = 1;
+	appInfo.pEngineName = "uwb-vk";
+	appInfo.engineVersion = 1;
+	appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
+
+
+	VkInstanceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	//instance extensionsv
+	auto extensions = getRequiredExtensions();
+	createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+	createInfo.ppEnabledExtensionNames = extensions.data();
+
+	//validation layers
+	if (enableValidationLayers) {
+		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	}
+	else {
+		createInfo.enabledLayerCount = 0;
+	}
+
+	if (vkCreateInstance(&createInfo, nullptr, &mInstance) != VK_SUCCESS) {
+		std::cerr << "Failed to create instance!" << std::endl;
+	}
+}
+
+void VulkanContext::createDevice(VkInstance instance)
+{
+	std::cout << "Creating Device" << std::endl;
+
+	//get a list of physical devices
+	uint32_t physicalDeviceCount;
+	vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, nullptr);
+
+	std::vector<VkPhysicalDevice> availableDevices(physicalDeviceCount);
+	vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, availableDevices.data());
+
+	//select one of the devices
+	std::cout << "Devices Found:" << physicalDeviceCount << std::endl;
+	for (const auto& device : availableDevices) {
+		printPhysicalDeviceDetails(device);
+	}
+
+	//just pick the first physical device for now
+	//we'll need to do device suitability checks later
+	physicalDevice = availableDevices[0];
+
+	selectedIndices = findQueueFamilies(physicalDevice, surface);
+
+	//create a queue for both graphics and presentation
+	std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+
+	//create a graphics queue
+	VkDeviceQueueCreateInfo graphicsQueueCreateInfo = {};
+	graphicsQueueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	graphicsQueueCreateInfo.queueFamilyIndex = selectedIndices.graphicsFamily;
+	graphicsQueueCreateInfo.queueCount = 1;
+	float queuePriority = 1.0f;
+	graphicsQueueCreateInfo.pQueuePriorities = &queuePriority;
+	queueCreateInfos.push_back(graphicsQueueCreateInfo);
+
+	//create a separate present queue if necessary (different family)
+	if (selectedIndices.graphicsFamily != selectedIndices.presentFamily) {
+		VkDeviceQueueCreateInfo presentQueueCreateInfo = {};
+		presentQueueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		presentQueueCreateInfo.queueFamilyIndex = selectedIndices.presentFamily;
+		presentQueueCreateInfo.queueCount = 1;
+		float queuePriority = 1.0f;
+		presentQueueCreateInfo.pQueuePriorities = &queuePriority;
+		queueCreateInfos.push_back(presentQueueCreateInfo);
+	}
+
+	//specify the features of the device we'll be using
+	VkPhysicalDeviceFeatures deviceFeatures = {};
+	deviceFeatures.samplerAnisotropy = VK_TRUE;
+
+	//main createInfo struct
+	VkDeviceCreateInfo deviceCreateInfo = {};
+	deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	deviceCreateInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+	deviceCreateInfo.pQueueCreateInfos = queueCreateInfos.data();
+	deviceCreateInfo.pEnabledFeatures = &deviceFeatures;
+
+	//extensions we want this device to use
+	deviceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
+	deviceCreateInfo.ppEnabledExtensionNames = deviceExtensions.data();
+
+	//create the device
+	if (vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS) {
+		std::runtime_error("Device Creation Failed");
+	}
+
+	//get the queue handles
+	vkGetDeviceQueue(device, selectedIndices.graphicsFamily, 0, &graphicsQueue);
+	vkGetDeviceQueue(device, selectedIndices.presentFamily, 0, &presentQueue);
+}
+
+void VulkanContext::createSurface(VkInstance instance, GLFWwindow* window)
+{
+	std::cout << "Creating Surface" << std::endl;
+
+	if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to create a window surface!");
+	}
+}
+
+void VulkanContext::printPhysicalDeviceDetails(VkPhysicalDevice device)
+{
+	VkPhysicalDeviceProperties properties;
+	vkGetPhysicalDeviceProperties(device, &properties);
+
+	std::cout << properties.deviceName << std::endl;
+	std::cout << "API Version: " << properties.apiVersion << std::endl;
+}
+
+std::vector<const char*> VulkanContext::getRequiredExtensions()
+{
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+	if (enableValidationLayers) {
+		extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+	}
+
+	return extensions;
+}
+
+void VulkanContext::setupDebugCallback()
+{
+	if (!enableValidationLayers) return;
+
+	std::cout << "Setting up debug callback" << std::endl;
+
+	VkDebugReportCallbackCreateInfoEXT createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+	createInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
+	createInfo.pfnCallback = debugCallback;
+
+	if (CreateDebugReportCallbackEXT(mInstance, &createInfo, nullptr, &mCallback) != VK_SUCCESS) {
+		throw std::runtime_error("failed to set up debug callback!");
+	}
+}
