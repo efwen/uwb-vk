923f8a8943b4a3a9205846c4e6901afaaee7adaa
Nick Carpenetti <nickcarpenetti@gmail.com>
2018-08-27 15:15:22 -0700

    Light Source Documentation

--- 12-ShadowMapping/Lighting.h
+++ 12-ShadowMapping/Lighting.h
@@ -1,8 +1,9 @@
 #pragma once
 
-const int MAX_LIGHTS = 8;
+const int MAX_LIGHTS = 8;			///< The maximum number of lights available
 
-//Enum for lightType, forced to use uint32_t
+/** @brief The type of light being used
+*/
 enum class LightType : uint32_t
 {
 	None = 0,
@@ -11,40 +12,49 @@ enum class LightType : uint32_t
 	Spot
 };
 
-//A struct containing the info necessary for any particular light
-//Note: member order is specific, as it is copied byte-by-byte
-//into a buffer, and the shaders also expect the same order
+
+/** @brief A Light Source
+
+	A struct containing the info necessary for any particular light
+	Note: member order is specific, as it is copied byte-by-byte
+	into a buffer, and the shaders also expect the same order
+
+	@author Nicholas Carpenetti
+
+	@date 9 Aug 2018
+*/
 struct Light
 {
-	//position and direction are both in world space
-	glm::vec4 position = glm::vec4(0.0);
-	glm::vec4 direction = glm::vec4(0.0, 0.0, -1.0, 1.0);
+	glm::vec4 position = glm::vec4(0.0);						///< Position of the light in world space
+	glm::vec4 direction = glm::vec4(0.0, 0.0, -1.0, 1.0);		///< Normalized forward vector for the light source
 
-	//light colors (default magent for debugging)
-	glm::vec4 ambient = glm::vec4(1.0, 0.0, 1.0, 1.0);
-	glm::vec4 diffuse = glm::vec4(1.0, 0.0, 1.0, 1.0);
-	glm::vec4 specular = glm::vec4(1.0, 0.0, 1.0, 1.0);
+	glm::vec4 ambient = glm::vec4(1.0, 0.0, 1.0, 1.0);			///< Ambient color of the light source
+	glm::vec4 diffuse = glm::vec4(1.0, 0.0, 1.0, 1.0);			///< Diffuse color of the light source
+	glm::vec4 specular = glm::vec4(1.0, 0.0, 1.0, 1.0);			///< Specular color of the light source
 
-	uint32_t isEnabled = false;
+	uint32_t isEnabled = false;									///< Is the light source enabled?
 
-	//attenuation parameters
-	float constant = 0.0f;
-	float linear = 0.0f;
-	float quadratic = 0.0f;
+	float constant = 0.0f;										///< Constant light attenuation
+	float linear = 0.0f;										///< Linear light attenuation
+	float quadratic = 0.0f;										///< Quadratic light attenuation
 
-	//cutoff values for spotlights (dot product values)
-	//this created to make the circle shape
-	float cutOff = 0.0f;
-	float outerCutOff = 0.0f;
-	LightType lightType = LightType::None;
+	float cutOff = 0.0f;										///< Inner Dot product cutoff value for spotlights
+	float outerCutOff = 0.0f;									///< Outer Dot product cutoff value for spotlights
+	LightType lightType = LightType::None;						///< The light type (dirctional, point, or spot)
 	
-	//bring the array up to a multiple of 32 bytes
-	char padding[4];							
+	char padding[4];											///< Padding values to fix alignment issues passing to the shader
 };
 
+
 //A UBO containing the information necessary to calculate lighting
+/** @brief Buffer information necessary to calculate lighting
+
+	@author Nicholas Carpenetti
+
+	@date 9 Aug 2018
+*/
 struct LightUBO
 {
-	glm::vec4 viewPos;			//the position we are viewing from (in world space)
-	Light lights[MAX_LIGHTS];	//array of lights we are using
+	glm::vec4 viewPos;			///< The position we are viewing from (in world space)
+	Light lights[MAX_LIGHTS];	///< The array of lights we are using
 };
