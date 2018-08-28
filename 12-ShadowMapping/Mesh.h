698e15a229fa06da4d6afa84c19dfdc3bd1f8109
Nick Carpenetti <nickcarpenetti@gmail.com>
2018-08-27 17:05:31 -0700

    Mesh class documentation

--- 12-ShadowMapping/Mesh.h
+++ 12-ShadowMapping/Mesh.h
@@ -12,29 +12,55 @@
 #include "BufferManager.h"
 #include "Vertex.h"
 
+/** @brief A Mesh composed of vertex and index buffers
+
+	@author Nicholas Carpenetti
+
+	@date 20 July 2018
+*/
 class Mesh
 {
 public:
+	/** @brief Constructor
+		@param context The VulkanContext object
+		@param bufferManager The BufferManager, 
+			used for making vertex and index buffers
+	*/
 	Mesh(std::shared_ptr<VulkanContext> context, std::shared_ptr<BufferManager> bufferManager);
 	~Mesh();
 	
+	/** @brief Create Vertex and index buffers from vectors of vertices and indices
+		@param vertices A vector of vertices
+		@param indices  A vector of indices
+	*/
 	void load(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices);
+	/** @brief Free all resources
+	*/
 	void free();
 
+	/** @brief get the number of indices
+		@return The number of indices
+	*/
 	uint32_t getIndexCount();
+	/** @brief Get a handle to the Vertex Buffer
+		@return The vertex buffer
+	*/
 	VkBuffer getVertexBuffer();
+	/** @brief Get a handle to the Index Buffer
+		@return The index buffer
+	*/
 	VkBuffer getIndexBuffer();
 protected:
-	std::shared_ptr<VulkanContext> mContext;
-	std::shared_ptr<BufferManager> mBufferManager;
+	std::shared_ptr<VulkanContext> mContext;			///< The RenderSystem's VulkanContext
+	std::shared_ptr<BufferManager> mBufferManager;		///< The RenderSystem's BufferManager
 
 	//vertex buffer
-	std::vector<Vertex> mVertices;
-	VkBuffer mVertexBuffer;
-	VkDeviceMemory mVertexBufferMemory;
+	std::vector<Vertex> mVertices;						///< The vertices in the mesh
+	VkBuffer mVertexBuffer;								///< The VkBuffer object for holding vertices
+	VkDeviceMemory mVertexBufferMemory;					///< The device memory for the vertices
 
 	//index buffer
-	std::vector<uint32_t> mIndices;
-	VkBuffer mIndexBuffer;
-	VkDeviceMemory mIndexBufferMemory;
+	std::vector<uint32_t> mIndices;						///< The indices in the mesh
+	VkBuffer mIndexBuffer;								///< The VkBuffer object for the indices
+	VkDeviceMemory mIndexBufferMemory;					///< The device memory for the indices
 };
